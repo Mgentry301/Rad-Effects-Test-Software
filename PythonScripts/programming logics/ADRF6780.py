1a5d9a805fe411281e971d962860d8ddab56d22c
EricMuire-analog <eric.muire@analog.com>
2025-10-24 14:03:57 -0400

    Most things ready for tamu nov 25

--- PythonScripts/programming logics/ADRF6780.py
+++ PythonScripts/programming logics/ADRF6780.py
@@ -1,11 +1,7 @@
 def logic(client):
-    """Custom logic for ADRF6780."""
     client.ContextPath = "\System\Subsystem_1\ADRF6780-042654 RevA\ADRF6780"
+    client.NavigateToPath("Root::System.Subsystem_1.ADRF6780-042654 RevA.ADRF6780")
+    client.Run("@Reset")
     client.SetBoolParameter("IQ_Mode_Enable", "False", "-1")
     client.SetBoolParameter("IF_Mode_Enable", "True", "-1")
     client.Run("@ApplySettings")
-    client.ReadRegister("0")
-    client.ReadRegister("3")
-    client.ReadRegister("6")
-    client.Run("@ReadSettings")
-    print("Custom logic for ADRF6780 executed.")
